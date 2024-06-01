464f7c3b43bed2d1f314c718d77e874159b07a1e
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-06-01 23:03:11 +0900

    Update vsftpd_create.sh

--- ftp/vsftpd_create.sh
+++ ftp/vsftpd_create.sh
@@ -18,3 +18,7 @@ systemctl stop vsftpd
 systemctl restart vsftpd
 # vsftpd 상태 확인
 systemctl status vsftpd
+
+# 계정 생성 및 권한 설정
+sudo useradd -r -m -d /upload -s /bin/bash ftpuser
+sudo chown -R ftpuser:ftpuser /upload
